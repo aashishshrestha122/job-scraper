b93ae9d8e211d6b32308fbf98d524079b6a77666
Aashish Shrestha <aashishshrestha12@gmail.com>
2023-12-21 09:33:39 +1100

    app.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,7 @@
-import { useEffect } from 'react';
-import axios from 'axios';
 import './App.css';
 import Home from './pages/home';
 
 function App() {
-  const SERVER_URL = 'http://localhost:3000/api/jobs/get-all-jobs';
-
-  useEffect(() => {
-    axios.post(SERVER_URL, {
-      page: '',
-      perPage: '50',
-      sortBy: 'id',
-      sortDirection: 'DESC'
-    })
-      .then(function (response) {
-        // handle success
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
-  }, [])
-
   return (
     <Home />
   );
